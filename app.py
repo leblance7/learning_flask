0ea57bfeb7d22c506d30d2aee971f36d46624a05
leblance7 <leblance7@gmail.com>
2025-04-14 19:29:48 -0500

    'Learning Python Tutorial' from freecodecamp.com

---
+++ app.py
@@ -0,0 +1,15 @@
+## Importing flask from Flask application.
+from flask import Flask, render_template
+
+## Setting the name name for the flask application.
+app = Flask(__name__)
+
+## Setting the website address for the website
+@app.route('/')
+
+def index():
+## Opening localhost:5000 is the developement browser.
+        return render_template("index.html")
+
+if __name__ == "__main__":
+        app.run(debug = "True")
